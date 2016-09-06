82eebdae6768fadeae6e15ac8faf3e63f5f2c4b5
Eric Larson <eclarson@erics-macbook-pro.local>
2016-09-06 12:16:51 -0500

    pedometer and activity print to log

--- Commotion/ViewController.swift
+++ Commotion/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController {
     
     // MARK: class variables
     let activityManager = CMMotionActivityManager()
+    let pedometer = CMPedometer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.startAcitivityMonitoring()
+        self.startActivityMonitoring()
+        self.startPedometerMonitoring()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,16 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func startAcitivityMonitoring(){
+    func startActivityMonitoring(){
+        // is activity is available
         if CMMotionActivityManager.isActivityAvailable(){
+            // update from this queue (should we use the MAIN queue here??.... )
             self.activityManager.startActivityUpdatesToQueue(NSOperationQueue.mainQueue())
             {(activity:CMMotionActivity?)->Void in
+                // unwrap the activity and dispaly
                 if let unwrappedActivity = activity {
                     print("%@",unwrappedActivity.description)
                 }
             }
         }
         
+    }
+    
+    func startPedometerMonitoring(){
+        if CMPedometer.isStepCountingAvailable(){
+            pedometer.startPedometerUpdatesFromDate(NSDate())
+            {(pedData:CMPedometerData?, error:NSError?)->Void in
+                if pedData != nil {
+                    print("\(pedData!.description)")
+                }
+            }
+        }
     }
 
 
