21537f955255d199029f6e6313f383ce5551dede
rongwei <rongwei.ji@outlook.com>
2023-10-11 13:05:45 -0500

    add scene class

---
+++ Commotion/FruitGameScene.swift
@@ -0,0 +1,16 @@
+//
+//  FruitGameScene.swift
+//  Commotion
+//
+//  Created by RongWei Ji on 10/11/23.
+//  Copyright © 2023 Eric Larson. All rights reserved.
+//
+
+import Foundation
+
+class FruitGameScene{
+    
+    
+    
+    
+}
