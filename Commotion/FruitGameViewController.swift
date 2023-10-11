9237c8b8ece11bf70d8e260d70d83320c16a2821
rongwei <rongwei.ji@outlook.com>
2023-10-11 12:48:34 -0500

    start ModuleB and create files.

---
+++ Commotion/FruitGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FruitGameViewController.swift
+//  Commotion
+//
+//  Created by RongWei Ji on 10/11/23.
+//  Copyright © 2023 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class FruitGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
